8dcd19695509f598303ba25b59a350a215370c3b
peilongchencc <peilongchencc@163.com>
2024-08-15 18:54:56 +0800

    去除trans_web_demo中无用的parse_text模块

--- basic_demo/trans_web_demo.py
+++ basic_demo/trans_web_demo.py
@@ -67,38 +67,6 @@ class StopOnTokens(StoppingCriteria):
         return False
 
 
-def parse_text(text):
-    lines = text.split("\n")
-    lines = [line for line in lines if line != ""]
-    count = 0
-    for i, line in enumerate(lines):
-        if "```" in line:
-            count += 1
-            items = line.split('`')
-            if count % 2 == 1:
-                lines[i] = f'<pre><code class="language-{items[-1]}">'
-            else:
-                lines[i] = f'<br></code></pre>'
-        else:
-            if i > 0:
-                if count % 2 == 1:
-                    line = line.replace("`", "\`")
-                    line = line.replace("<", "&lt;")
-                    line = line.replace(">", "&gt;")
-                    line = line.replace(" ", "&nbsp;")
-                    line = line.replace("*", "&ast;")
-                    line = line.replace("_", "&lowbar;")
-                    line = line.replace("-", "&#45;")
-                    line = line.replace(".", "&#46;")
-                    line = line.replace("!", "&#33;")
-                    line = line.replace("(", "&#40;")
-                    line = line.replace(")", "&#41;")
-                    line = line.replace("$", "&#36;")
-                lines[i] = "<br>" + line
-    text = "".join(lines)
-    return text
-
-
 def predict(history, prompt, max_length, top_p, temperature):
     stop = StopOnTokens()
     messages = []
@@ -160,11 +128,11 @@ with gr.Blocks() as demo:
 
 
     def user(query, history):
-        return "", history + [[parse_text(query), ""]]
+        return "", history + [[query, ""]]
 
 
     def set_prompt(prompt_text):
-        return [[parse_text(prompt_text), "成功设置prompt"]]
+        return [[prompt_text, "成功设置prompt"]]
 
 
     pBtn.click(set_prompt, inputs=[prompt_input], outputs=chatbot)
